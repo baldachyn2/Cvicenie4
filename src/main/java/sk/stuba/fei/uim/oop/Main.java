3515df1cdb9652d5effd36954e8c78789fa680d7
Matúš Švantner <xsvantner@stuba.sk>
2022-03-19 09:30:29 +0100

    ArrayList

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,11 +1,29 @@
 package sk.stuba.fei.uim.oop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        zivocich clovek = new clovek();
-        zivocich pes = new pes();
+        List<String> arraylist = new ArrayList<>();
+        arraylist.add(0,"bengoro");
+        arraylist.add(1,"skr skr");
+        for (var i : arraylist){
+            System.out.println(i);
+        }
+        List<Integer> arraylist2 = new ArrayList<>();
+        arraylist2.add(0,146);
+        arraylist2.add(0,0);
 
-        clovek.zvuk();
-        pes.zvuk();
+        for (var i : arraylist2){
+            System.out.println(i);
+        }
+
+        ArrayList<pes> pes = new ArrayList<>();
+        pes.add(new pes("dunco"));
+        pes.add(new pes("bengorbarber"));
+        for (var i : pes){
+            System.out.println(i);
+        }
     }
 }
--- src/main/java/sk/stuba/fei/uim/oop/pes.java
+++ src/main/java/sk/stuba/fei/uim/oop/pes.java
@@ -1,6 +1,15 @@
 package sk.stuba.fei.uim.oop;
 
 public class pes extends zivocich {
+    String name;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public pes(String name) {
+        this.name = name;
+    }
+
     @Override
     protected void zvuk() {
         System.out.println("pes");
